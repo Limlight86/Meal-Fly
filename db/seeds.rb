c86632f1922d67652cf6e1e990241a0ae3bceab2
Laz <lquin008@gmail.com>
2019-05-25 16:48:45 -0400

    additional files created during inital work

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,54 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.destroy_all
+Recipe.destroy_all
+RecipeCategory.destroy_all
+
+categories = [
+              {name: 'latin'},
+              {name: 'japanese'},
+              {name: 'italian'},
+              {name: 'comfort food'},
+              {name: 'dessert'},
+              {name: 'healthy'},
+              {name: 'vegan'}
+             ]
+
+categories.each { |category| Category.create(category) }
+
+eggs = Recipe.create(
+               name: "Fried Eggs",
+               servings: 4,
+               difficulty: 1,
+               calories: 94,
+               carbs: 0,
+               protein: 6,
+               fat: 5,
+               sugar: 0,
+               fiber: 0,
+               vegan: false,
+               gluten_free: true,
+               image_url: "https://i.stack.imgur.com/ZuAmS.jpg"
+)
+
+arroz_pollo = Recipe.create(
+                name: "Arroz con Pollo",
+                servings: 4,
+                difficulty: 2,
+                calories: 430,
+                carbs: 50,
+                protein: 20,
+                fat: 16,
+                sugar: 1,
+                fiber: 2,
+                vegan: false,
+                gluten_free: true,
+                image_url: "https://www.dinneratthezoo.com/wp-content/uploads/2015/05/arroz-con-pollo-4.jpg"
+)
+
+RecipeCategory.create(recipe_id: eggs.id, category_id: Category.comfort_food.id)
+RecipeCategory.create(recipe_id: arroz_pollo.id, category_id: Category.comfort_food.id)
+RecipeCategory.create(recipe_id: arroz_pollo.id, category_id: Category.latin.id)
+#pass IDs to imrove performance
+
+puts "#{Recipe.count} recipes and"
+puts "#{Category.count} categories and"
+puts "#{RecipeCategory.count} entries in Recipe Category."
