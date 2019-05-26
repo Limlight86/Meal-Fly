a126737e0802312ebf6e4d99b4d369854fe7de6c
Laz <lquin008@gmail.com>
2019-05-25 20:30:50 -0400

    added destroy functionality to the all recipes page and simple alerts

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -18,10 +18,17 @@ class RecipesController < ApplicationController
       RecipeCategory.create(recipe: @recipe, category: Category.find(category.to_i))
     end
     if @recipe.save
-      redirect_to @recipe
+      redirect_to @recipe, notice: "Recipe was successfully created"
     else
       render 'new'
     end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+   
+    redirect_to '/recipes', notice: 'Recipe was successfully destroyed.'
   end
 
   private
