07c326f74ea31aef255eb0c145132e40191e3362
Andy Weiss <andyweiss1982@gmail.com>
2019-05-28 09:54:36 -0400

    replace dropdowns with checkboxes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,11 @@
-class RecipesController < ApplicationController 
+class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+
   def index
     @recipes = Recipe.all
   end
 
   def show
-    @recipe = Recipe.find(params[:id])
   end
 
   def new
@@ -14,9 +15,7 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.new(recipe_params)
-    params[:recipe][:categories].reject!(&:empty?).each do |category| 
-      RecipeCategory.create(recipe: @recipe, category: Category.find(category.to_i))
-    end
+    assign_categories
     if @recipe.save
       redirect_to @recipe, notice: "Recipe was successfully created"
     else
@@ -25,19 +24,11 @@ class RecipesController < ApplicationController
   end
 
   def edit
-    @recipe = Recipe.find(params[:id])
     @categories = Category.all
   end
 
   def update
-    @recipe = Recipe.find(params[:id])
-    @categories = Category.all
-    categories = params[:recipe][:categories].reject!(&:empty?)
-    @recipe.recipe_categories.destroy_all if !categories.empty?
-    categories.each do |category| 
-      RecipeCategory.create(recipe: @recipe, category: Category.find(category.to_i))
-    end
-   
+    assign_categories
     if @recipe.update(recipe_params)
       redirect_to @recipe, notice: "Recipe was successfully updated"
     else
@@ -46,15 +37,26 @@ class RecipesController < ApplicationController
   end
 
   def destroy
-    @recipe = Recipe.find(params[:id])
     @recipe.destroy
-   
     redirect_to recipes_url, notice: 'Recipe was successfully destroyed.'
   end
 
   private
 
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def assign_categories
+    @recipe.recipe_categories.destroy_all
+    params[:recipe][:categories].each do |category_id, checked|
+      if checked == "1"
+        RecipeCategory.create(recipe: @recipe, category_id: category_id)
+      end
+    end
+  end
+
   def recipe_params
-    params.require(:recipe).permit(:name, :servings, :difficulty, :calories, :carbs, :protein, :fat, :sugar, :fiber, :vegan, :gluten_free, :image_url, :categories, :recipe_details, :recipe, :video_url, :cooktime)
+    params.require(:recipe).permit(:name, :servings, :difficulty, :calories, :carbs, :protein, :fat, :sugar, :fiber, :vegan, :gluten_free, :image_url, :recipe_details, :recipe, :video_url, :cooktime)
   end
 end
