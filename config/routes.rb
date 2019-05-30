5dc22ad30b5dbff9eaf145be31864c5334090dee
Lazaro <lquin008@gmail.com>
2019-05-30 19:05:06 -0400

    added user functionality to app and adjusted the css by using bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'recipes#index'
+  devise_for :users
   resources :recipes
   resources :categories
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
