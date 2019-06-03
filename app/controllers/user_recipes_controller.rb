47757180affb1ec528e650d51ccae5811ac4b405
Andy Weiss <andyweiss1982@gmail.com>
2019-06-03 14:20:48 -0400

    prepare routes and controller actions for react button

--- app/controllers/user_recipes_controller.rb
+++ app/controllers/user_recipes_controller.rb
@@ -1,31 +1,23 @@
 class UserRecipesController < ApplicationController
-  before_action :set_user_recipe, only: [:destroy]
+  before_action :set_recipe, only: [:create, :destroy]
   
   def index
     @user_recipes = current_user.user_recipes.includes(:recipe)
   end
 
   def create
-    @user_recipe = current_user.user_recipes.new(recipe_id: params[:recipe_id])
-    if @user_recipe.save
-      flash[:notice] = "Recipe was successfully added to Cookbook"
-    else
-      flash[:alert] = "Recipe already in your Cookbook."
-    end
-    redirect_back fallback_location: root_path
+    current_user.user_recipes.find_or_create_by(recipe: @recipe)
+    redirect_back fallback_location: root_path, notice: "Recipe added to cookbook"
   end
 
   def destroy
-    @user_recipe.destroy
-    redirect_back fallback_location: root_path, notice: 'Recipe was successfully removed from Cookbook.'
+    current_user.user_recipes.where(recipe: @recipe).destroy_all
+    redirect_back fallback_location: root_path, notice: 'Recipe removed from Cookbook.'
   end
 
   private
-  def set_user_recipe
-    @user_recipe = UserRecipe.find(params[:id])
-  end
 
-  def user_recipe_params
-    params.require(:user_recipe).permit(:id, :recipe)
+  def set_recipe
+    @recipe = Recipe.find(params[:recipe_id])
   end
 end
