3b9be68f8ea78c64915489aa413967e6d7b0046e
Lazaro <lquin008@gmail.com>
2019-06-04 20:56:50 -0400

    further seed changes to facilitate front end shows

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@ user = User.create(email: "some@guy.com", password: "password", username: "someg
 admin = User.create(email: "admin@gmail.com", password: "redfred", admin: true, username: "admin")
 
 categories  = [
-                {name: 'latin'},
-                {name: 'japanese'},
-                {name: 'italian'},
-                {name: 'comfort food'},
-                {name: 'dessert'},
-                {name: 'healthy'},
-                {name: 'vegan'},
-                {name: 'gluten free'}
+                {name: 'Latin'},
+                {name: 'Japanese'},
+                {name: 'Italian'},
+                {name: 'Comfort food'},
+                {name: 'Dessert'},
+                {name: 'Healthy'},
+                {name: 'Vegan'},
+                {name: 'Gluten-free'}
               ]
 
 categories.each { |category| Category.create(category) }
@@ -102,18 +102,18 @@ cinnamon_rolls = Recipe.create(
   recipe_details: "Cook the food, Enjoy the food!...No animals were harmed in the making of this recipe!"
 )
 
-RecipeCategory.create(recipe: eggs, category: Category.find_by(name: "comfort food"))
-RecipeCategory.create(recipe: arroz_pollo, category: Category.find_by(name: "comfort food"))
-RecipeCategory.create(recipe: arroz_pollo, category: Category.find_by(name: "latin"))
-RecipeCategory.create(recipe: ramen, category: Category.find_by(name: "japanese"))
-RecipeCategory.create(recipe: lasgna, category: Category.find_by(name: "italian"))
-RecipeCategory.create(recipe: lasgna, category: Category.find_by(name: "comfort food"))
-RecipeCategory.create(recipe: tofu_nuggets, category: Category.find_by(name: "comfort food"))
-RecipeCategory.create(recipe: tofu_nuggets, category: Category.find_by(name: "healthy"))
-RecipeCategory.create(recipe: tofu_nuggets, category: Category.find_by(name: "vegan"))
-RecipeCategory.create(recipe: cinnamon_rolls, category: Category.find_by(name: "vegan"))
-RecipeCategory.create(recipe: cinnamon_rolls, category: Category.find_by(name: "dessert"))
-RecipeCategory.create(recipe: cinnamon_rolls, category: Category.find_by(name: "gluten free"))
+RecipeCategory.create(recipe: eggs, category: Category.find_by(name: "Comfort food"))
+RecipeCategory.create(recipe: arroz_pollo, category: Category.find_by(name: "Comfort food"))
+RecipeCategory.create(recipe: arroz_pollo, category: Category.find_by(name: "Latin"))
+RecipeCategory.create(recipe: ramen, category: Category.find_by(name: "Japanese"))
+RecipeCategory.create(recipe: lasgna, category: Category.find_by(name: "Italian"))
+RecipeCategory.create(recipe: lasgna, category: Category.find_by(name: "Comfort food"))
+RecipeCategory.create(recipe: tofu_nuggets, category: Category.find_by(name: "Comfort food"))
+RecipeCategory.create(recipe: tofu_nuggets, category: Category.find_by(name: "Healthy"))
+RecipeCategory.create(recipe: tofu_nuggets, category: Category.find_by(name: "Vegan"))
+RecipeCategory.create(recipe: cinnamon_rolls, category: Category.find_by(name: "Vegan"))
+RecipeCategory.create(recipe: cinnamon_rolls, category: Category.find_by(name: "Dessert"))
+RecipeCategory.create(recipe: cinnamon_rolls, category: Category.find_by(name: "Gluten-free"))
 
 UserRecipe.create(recipe: arroz_pollo, user: user)
 UserRecipe.create(recipe: lasgna, user: user)
